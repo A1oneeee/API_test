1c4471c852eba9dd139c0382067fd52321e85c84
Valoch <116378179+A1oneeee@users.noreply.github.com>
2024-01-03 09:34:03 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+document.querySelector('#fetch-dog').addEventListener('click', async() => {
+	refreshDog();
+});
+
+async function refreshDog() {
+	var fetchResult = await fetch('https://random.dog/woof.json');
+	var data = await fetchResult.json();
+	
+	if(data.url.includes('.mp4')){
+		refreshDog();
+		return;
+	}
+	document.querySelector('#img-dog').src = data.url;
+}
